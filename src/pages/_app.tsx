1e32aa57894fc33bcbb653320a0f8114cdef7657
Snahier <hansalmeida@hotmail.com>
2021-03-07 20:00:50 -0300

    Add styled components theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "../styles/global"
+import { light } from "../styles/theme"
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={light}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
