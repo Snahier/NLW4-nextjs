4fed100052ccaa6ec677590b6f4d414dfb87c3ce
Snahier <hansalmeida@hotmail.com>
2021-03-07 20:56:06 -0300

    Add home grid layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,27 @@
+import styled from "styled-components/macro"
 import { ExperienceBar } from "../components/ExperienceBar"
+import { Profile } from "../components/Profile"
 
 export default function Home() {
   return (
     <div className="container">
       <ExperienceBar />
+
+      <Section>
+        <div>
+          <Profile />
+        </div>
+        <div></div>
+      </Section>
     </div>
   )
 }
+
+const Section = styled.section`
+  flex: 1;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 6.25rem;
+  align-items: center;
+`
