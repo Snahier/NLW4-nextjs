1476dc0ff2e37ebda37c77770262e858a10f3b63
Snahier <hansalmeida@hotmail.com>
2021-03-07 01:16:22 -0300

    Add theme file

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const light = {
+  colors: {
+    white: "#fff",
+    red: "#e83f5b",
+    green: "#4cd62b",
+    blue: "#5965e0",
+    blueDark: "#4953b8",
+    blueTwitter: "#2aa9e0",
+  },
+
+  title: "#2e384d",
+  text: "#666",
+  textHighlight: "#b3b9ff",
+  grayLine: "#dcdde0",
+
+  fonts: {
+    inter: "Inter",
+  },
+}
