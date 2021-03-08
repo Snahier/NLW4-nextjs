6d3b10d66463ad2ccfb6ae1ba224107b57ca01ae
Snahier <hansalmeida@hotmail.com>
2021-03-08 00:08:37 -0300

    Add experience to next level number

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -11,8 +11,10 @@ interface ChallengesContextData {
   currentExperience: number
   challengesCompleted: number
   activeChallenge: Challenge
+  experienceToNextLevel: number
   levelUp: () => void
   startNewChallenge: () => void
+  resetChallenge: () => void
 }
 
 interface ChallengesProviderProps {
@@ -23,10 +25,13 @@ export const ChallengesContext = createContext({} as ChallengesContextData)
 
 export const ChallengesProvider = ({ children }) => {
   const [level, setLevel] = useState(1)
-  const [currentExperience, setCurrentExperience] = useState(0)
+  const [currentExperience, setCurrentExperience] = useState(3)
 
   const [challengesCompleted, setChallengesCompleted] = useState(0)
   const [activeChallenge, setActiveChallenge] = useState(null)
+
+  const experienceFactor = 4
+  const experienceToNextLevel = ((level + 1) * experienceFactor) ** 2
 
   const levelUp = () => setLevel(level + 1)
 
@@ -37,15 +42,19 @@ export const ChallengesProvider = ({ children }) => {
     setActiveChallenge(challenge)
   }
 
+  const resetChallenge = () => setActiveChallenge(null)
+
   return (
     <ChallengesContext.Provider
       value={{
         level,
         currentExperience,
-        challengesCompleted,
+        experienceToNextLevel,
         activeChallenge,
+        challengesCompleted,
         levelUp,
         startNewChallenge,
+        resetChallenge,
       }}
     >
       {children}
